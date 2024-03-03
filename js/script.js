26dfe036229fad4ff56998ce4bf2f50a432f4891
Olexander <alex.pilyavskiy03@gmail.com>
2024-03-03 17:37:42 +0200

    create fetch response

--- js/script.js
+++ js/script.js
@@ -63,3 +63,57 @@ document.addEventListener("click", function (e) {
     tabItems[newActiveIndex].classList.add("active");
   }
 });
+
+let userListVisible = false;
+
+async function fetchUsers() {
+  const userInput = document
+    .getElementById("userInput")
+    .value.trim()
+    .toLowerCase();
+  const userList = document.getElementById("userList");
+  const response = await fetch(`https://jsonplaceholder.typicode.com/users`);
+  const data = await response.json();
+  const sortedUsers = data.sort((a, b) => a.name.localeCompare(b.name));
+
+  userList.innerHTML = "";
+  if (userInput === "" && !userListVisible) {
+    userList.innerHTML = "";
+  } else {
+    const filteredUsers = sortedUsers.filter((user) =>
+      user.name.toLowerCase().includes(userInput)
+    );
+
+    if (filteredUsers.length > 0) {
+      filteredUsers.forEach((user) => {
+        const listItem = document.createElement("li");
+        listItem.textContent = user.name;
+        listItem.addEventListener("click", function () {
+          document.getElementById("userInput").value = user.name;
+          userListVisible = false;
+          userList.innerHTML = "";
+        });
+        userList.appendChild(listItem);
+      });
+    } else {
+      const noUsersMessage = document.createElement("li");
+      noUsersMessage.textContent = "No Users!";
+      noUsersMessage.style.color = "red";
+      userList.appendChild(noUsersMessage);
+    }
+  }
+}
+
+function showUserList() {
+  userListVisible = true;
+  fetchUsers();
+}
+
+document.addEventListener("click", function (event) {
+  const userInput = document.getElementById("userInput");
+  const userList = document.getElementById("userList");
+  if (!userInput.contains(event.target) && !userList.contains(event.target)) {
+    userListVisible = false;
+    userList.innerHTML = "";
+  }
+});
